373cbea85eee5b381ca25d60b632d68dd8f78ee5
David Perez <davidperez@MacBook-Pro-de-David.local>
2021-05-09 23:44:34 -0500

    Fix heroku bug

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.get('/posts/:post', function (req, res) {
 	});
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
 	console.log('Server started on port 3000');
 });
